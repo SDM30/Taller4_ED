71e84ffe85905473a1b4ddff91ec02e506602db1
DiegoAVH <166579135+DiegoAVH@users.noreply.github.com>
2024-11-02 19:39:32 -0500

    Update main.cxx
    Cambio en el main de los tiempos de carga y calculo de seis grados

--- main.cxx
+++ main.cxx
@@ -1,8 +1,9 @@
 
 #include "sistema.h"
+#include <chrono>
 
 int main(int argc, char *argv[]) {
-
+    
     Sistema control;
 
     if ( (argc < 3) || (argc > 3) ) {
@@ -10,8 +11,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    auto start = std::chrono::high_resolution_clock::now();
     control.cargarPersonas(argv[1], true);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Tiempo de ejecucion de carga: " << duration.count() << " microsegundos" << std::endl
+
+    auto start_seis_grados = std::chrono::high_resolution_clock::now();
     control.probarSeisGrados(argv[2], true);
+    auto end_seis_grados = std::chrono::high_resolution_clock::now();
+    auto duration_seis_grados = std::chrono::duration_cast<std::chrono::microseconds>(end_seis_grados - start_seis_grados);
+    std::cout << "Tiempo de ejecucion de seis grados: " << duration_seis_grados.count() << " microsegundos" << std::endl;
 
     return 0;
 }
